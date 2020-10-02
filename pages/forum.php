5b188ad7091db364bb0b468eb47877a115bddfea
romainleny <lenyromain1@gmail.com>
2020-10-02 16:33:19 +0200

    Add login and subscribe foms

--- pages/forum.php
+++ pages/forum.php
@@ -4,22 +4,61 @@ include('../includes/utils.inc.php');
 start_page('forum');
 ?>
 
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<div class="container">
+    <div class="d-flex justify-content-center">
+        <div class="border rounded p-5 mt-3 w-75">
+            <h1>Se connecter</h1>
+            <form action="login.php" method="post">
+            <div class="form-group col-md-8">
+                <label for="email_connexion">Email</label>
+                <input type="email" class="form-control" name="email_connexion" id="email_connexion" placeholder="email">
+            </div>
+            <div class="form-group col-md-8">
+                <label for="password_connexion">Mot de passe</label>
+                <input type="password" class="form-control" name="password_connexion" id="password_connexion" placeholder="mot de passe">
+            </div>
+            <div class="d-flex justify-content-center">
+                <input type="submit" class="btn btn-primary" name="connect" value="connexion">
+            </div>
+            </form>
+        </div>
+    </div>
+    <p class="text-center">Vous n'avez pas de compte ?<a class="" data-toggle="collapse" href="#collapseSubscribe" role="button" aria-expanded="false" aria-controls="collapseSubscribe"> Inscrivez-vous ici</a></p>
+    <div class="collapse mb-3" id="collapseSubscribe">
+    <div class="card card-body p-5">
+        <h1>S'inscrire</h1>
+        <form action="subscribe.php" method="post">
+            <div class="form-group col-md-8">
+                <label for="firstname">Nom</label>
+                <input type="text" class="form-control" name="firstname" id="firstname" placeholder="nom">
+            </div>
+            <div class="form-group col-md-8">
+                <label for="name">Prénom</label>
+                <input type="text" class="form-control" name="name" id="name" placeholder="prénom">
+            </div>
+            <div class="form-group col-md-8">
+                <label for="pseudo">Pseudo</label>
+                <input type="text" class="form-control" name="pseudo" id="pseudo" placeholder="pseudo">
+            </div>
+            <div class="form-group col-md-8">
+                <label for="email_subscribe">Email</label>
+                <input type="email" class="form-control" name="email_subscribe" id="email_subscribe" placeholder="email">
+            </div>
+            <div class="form-group col-md-8">
+                <label for="password_subscribe">Mot de passe</label>
+                <input type="password" class="form-control" name="password_subscribe" id="password_subscribe" placeholder="mot de passe">
+            </div>
+            <div class="form-group col-md-8">
+                <label for="confirm_password">Confirmation mot de passe</label>
+                <input type="password" class="form-control" name="confirm_password" id="confirm_password" placeholder="vérifier le mot de passe">
+            </div>
+            <div class="d-flex justify-content-center">
+                <input type="submit" class="btn btn-primary" name="subscribe" value="Inscription">
+            </div>
+        </form>
+    </div>
+    </div>
+</div>
 
 <?php
 end_page();
